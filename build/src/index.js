b948a9ff6e494baa5d7c57bb1e8560874c2ed719
Fishcake <me@fshck.com>
2023-07-08 18:08:10 +0900

    update to use with legacy account page

--- build/src/index.js
+++ build/src/index.js
@@ -6,10 +6,6 @@ import Audio from '@uppy/audio';
 //import Compressor from '@uppy/compressor';
 //import ImageEditor from '@uppy/image-editor';
 import Webcam from '@uppy/webcam';
-// AlpineJS
-// import Alpine from 'alpinejs'
-// window.Alpine = Alpine
-// Alpine.start()
 
 
 import '@uppy/core/dist/style.min.css';
@@ -19,6 +15,7 @@ import '@uppy/audio/dist/style.min.css';
 import '@uppy/webcam/dist/style.min.css';
 
 
+/*
 function formatBytes(bytes, decimals = 2) {
   if (bytes === 0) return '0 Bytes';
 
@@ -88,7 +85,10 @@ function createMediaLiElem(response) {
 
   return element;
 }
+*/
 
+
+/*
 
 // Uppy.js
 const uppy = new Uppy({
@@ -112,11 +112,6 @@ const uppy = new Uppy({
   },
 })
   .use(GoldenRetriever)
-  /*
-  .use(Compressor, {
-    quality: 0.6,
-  })
-  */
   .use(Dashboard, {
     target: '#files-drag-drop',
     inline: false,
@@ -130,12 +125,6 @@ const uppy = new Uppy({
   })
   .use(Webcam, { target: Dashboard })
   .use(Audio, { target: Dashboard })
-  /*
-  .use(ImageEditor, {
-    target: Dashboard,
-    quality: 0.6
-  })
-  */
   .use(XHRUpload, {
     endpoint: '/api/v2/account/files/uppy',
     method: 'post',
@@ -197,4 +186,98 @@ const uppy = new Uppy({
       folderName: folderName,
       folderHierarchy: folderHierarchy,
     });
+  })
+  */
+
+const uppy = new Uppy({
+  debug: false,
+  allowedFileTypes: ['image/*', 'video/*', 'audio/*'],
+  //maxFileSize: 15 * 1024 * 1024,
+  //maxTotalFileSize: 150 * 1024 * 1024,
+  onBeforeFileAdded: (currentFile, files) => {
+    const allowedTypes = ['video', 'audio', 'image'];
+    const fileType = currentFile.type.split('/')[0]; // Extract the file type from the MIME type
+
+    if (!allowedTypes.includes(fileType)) {
+      // log to console
+      uppy.log(`Skipping file ${currentFile.name} because it's not a video, audio, or image`);
+      // show error message to the user
+      uppy.info(`Skipping file ${currentFile.name} because it's not a video, audio, or image`, 'error', 500);
+      return false; // Exclude the file
+    }
+
+    return true; // Include the file
+  },
+})
+  .use(GoldenRetriever)
+  .use(Dashboard, {
+    target: '#files-account-dropzone',
+    inline: false,
+    trigger: '#open-account-dropzone-button',
+    showLinkToFileUploadResult: true,
+    showProgressDetails: true,
+    note: 'Images, video and audio only, up to 15 MB each',
+    fileManagerSelectionType: 'both',
+    proudlyDisplayPoweredByUppy: false,
+    theme: 'dark',
+    closeAfterFinish: true,
+  })
+  .use(Webcam, { target: Dashboard })
+  .use(Audio, { target: Dashboard })
+  .use(XHRUpload, {
+    endpoint: '/api/v2/account/files/uppy',
+    method: 'post',
+    formData: true,
+    bundle: false,
+    limit: 5,
+    meta: {
+      folderName: null, // Initialize folderName metadata
+      folderHierarchy: null, // Initialize folderHierarchy metadata
+    },
+  })
+  .on('upload-success', (file, response) => {
+    if (Array.isArray(response.body)) {
+      const fileResponse = response.body.find(f => f.id === file.id);
+      if (fileResponse) {
+        uppy.setFileMeta(file.id, {
+          name: fileResponse.name,
+          type: fileResponse.type,
+          size: fileResponse.size
+        });
+      }
+    } else {
+      uppy.setFileMeta(file.id, {
+        name: response.body.name,
+        type: response.body.type,
+        size: response.body.size
+      });
+    }
+  })
+  .on('upload-success', (file, response) => {
+    console.log('Upload result:', response);
+  })
+  .on('file-added', (file) => {
+    //console.log('Added file', file);
+    const path = file.data.relativePath ?? file.data.webkitRelativePath;
+    let folderHierarchy = [];
+    let folderName = null;
+
+    if (path) {
+      const folderPath = path.replace(/\\/g, '/'); // Normalize backslashes to forward slashes
+      const folderPathParts = folderPath.split('/').filter(part => part !== '');
+      folderHierarchy = folderPathParts.length > 1 ? folderPathParts.slice(0, -1) : [];
+      folderName = folderHierarchy.length > 0 ? folderHierarchy[folderHierarchy.length - 1] : null;
+    }
+    console.log('Folder name', folderName);
+    console.log('Folder hierarchy', folderHierarchy);
+    uppy.setFileMeta(file.id, {
+      folderName: folderName,
+      folderHierarchy: folderHierarchy,
+    });
+  })
+  .on('complete', (result) => {
+    // If the failed upload count is zero, all uploads succeeded
+    if (result.failed.length === 0) {
+      location.reload(); // reload the page
+    }
   })
