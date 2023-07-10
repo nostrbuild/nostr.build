fa9f9436510af6d948b4dc2c1d978f048ae33e16
Fishcake <me@fshck.com>
2023-07-10 16:28:07 +0900

    SQL can do many things, so lets use at least some of it

--- libs/db/UsersImagesFolders.class.php
+++ libs/db/UsersImagesFolders.class.php
@@ -32,6 +32,80 @@ class UsersImagesFolders extends DatabaseTable
     return $result->fetch_all(MYSQLI_ASSOC);
   }
 
+  public function getFoldersStats(string $usernpub): array
+  {
+    // Populate information about folders and files
+    $query = "
+        SELECT
+        uif.id,
+        uif.folder,
+        SUM(COALESCE(ui.file_size, 0)) AS totalSize,
+        COUNT(ui.id) AS fileCount,
+        COALESCE(SUM(CASE WHEN SUBSTRING_INDEX(ui.image, '.', -1) = 'gif' THEN 1 ELSE 0 END), 0) AS gifCount,
+        COALESCE(SUM(CASE WHEN SUBSTRING_INDEX(ui.image, '.', -1) IN ('mov', 'mp4', 'mp3') THEN 1 ELSE 0 END), 0) AS avCount,
+        COALESCE(SUM(CASE WHEN SUBSTRING_INDEX(ui.image, '.', -1) NOT IN ('gif', 'mov', 'mp4', 'mp3') THEN 1 ELSE 0 END), 0) AS imageCount,
+        COALESCE(SUM(CASE WHEN ui.flag = 1 THEN 1 ELSE 0 END), 0) AS publicCount
+    FROM users_images_folders uif
+    LEFT JOIN users_images ui ON uif.id = ui.folder_id
+        AND ui.image != 'https://nostr.build/p/Folder.png'
+    WHERE uif.usernpub = ?
+    GROUP BY uif.id, uif.folder
+    UNION ALL
+    SELECT
+        NULL AS id,
+        '/' AS folder,
+        COALESCE(SUM(COALESCE(ui.file_size, 0)), 0) AS totalSize,
+        COUNT(*) AS fileCount,
+        COALESCE(SUM(CASE WHEN SUBSTRING_INDEX(ui.image, '.', -1) = 'gif' THEN 1 ELSE 0 END), 0) AS gifCount,
+        COALESCE(SUM(CASE WHEN SUBSTRING_INDEX(ui.image, '.', -1) IN ('mov', 'mp4', 'mp3') THEN 1 ELSE 0 END), 0) AS avCount,
+        COALESCE(SUM(CASE WHEN SUBSTRING_INDEX(ui.image, '.', -1) NOT IN ('gif', 'mov', 'mp4', 'mp3') THEN 1 ELSE 0 END), 0) AS imageCount,
+        COALESCE(SUM(CASE WHEN ui.flag = 1 THEN 1 ELSE 0 END), 0) AS publicCount
+    FROM users_images ui
+    WHERE ui.usernpub = ?
+        AND ui.folder_id IS NULL
+        AND ui.image != 'https://nostr.build/p/Folder.png'
+    UNION ALL
+    SELECT
+        dummy.id,
+        dummy.folder,
+        COALESCE(SUM(COALESCE(ui.file_size, 0)), 0) AS totalSize,
+        COUNT(ui.id) AS fileCount,
+        COALESCE(SUM(CASE WHEN SUBSTRING_INDEX(ui.image, '.', -1) = 'gif' THEN 1 ELSE 0 END), 0) AS gifCount,
+        COALESCE(SUM(CASE WHEN SUBSTRING_INDEX(ui.image, '.', -1) IN ('mov', 'mp4', 'mp3') THEN 1 ELSE 0 END), 0) AS avCount,
+        COALESCE(SUM(CASE WHEN SUBSTRING_INDEX(ui.image, '.', -1) NOT IN ('gif', 'mov', 'mp4', 'mp3') THEN 1 ELSE 0 END), 0) AS imageCount,
+        COALESCE(SUM(CASE WHEN ui.flag = 1 THEN 1 ELSE 0 END), 0) AS publicCount
+    FROM (
+        SELECT NULL AS id, 'TOTAL' AS folder
+    ) AS dummy
+    LEFT JOIN users_images ui ON ui.usernpub = ?
+        AND ui.image != 'https://nostr.build/p/Folder.png'
+    GROUP BY dummy.id, dummy.folder
+      ";
+
+    $stmt = $this->db->prepare($query);
+    $stmt->bind_param('sss', $usernpub, $usernpub, $usernpub);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    $account_folders_data = [];
+    $folderCount = 0;
+
+    while ($row = $result->fetch_assoc()) {
+      if ($row['folder'] === 'TOTAL') {
+        $account_folders_data['TOTAL'] = $row;
+      } else {
+        if ($row['folder'] !== '/') {
+          $folderCount++;
+        }
+        $account_folders_data['FOLDERS'][$row['folder']] = $row;
+      }
+    }
+    // add folderCount to the TOTAL
+    $account_folders_data['TOTAL']['folderCount'] = $folderCount;
+    $stmt->close();
+    return $account_folders_data;
+  }
+
   public function findFolderByNameOrCreate(string $folder_name): int
   {
     $this->db->begin_transaction();
