d539890c0ce6ca2a33f9c46323bb4e3c9a89e16d
Fishcake <me@fshck.com>
2023-07-25 21:26:11 +0900

    add code to create invoices

--- libs/BTCPayClient.class.php
+++ libs/BTCPayClient.class.php
@@ -6,7 +6,8 @@ require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
 // Import Invoice client class.
 use BTCPayServer\Client\Invoice;
-use BTCPayServer\Client\Webhook;
+use BTCPayServer\Client\InvoiceCheckoutOptions;
+use BTCPayServer\Util\PreciseNumber;
 
 class BTCPayClient
 {
@@ -14,22 +15,50 @@ class BTCPayClient
   private $apiKey;
   private $host;
   private $storeId;
-  private $secret;
   private $invoice;
-  private $webhook;
+  private $currency;
 
-  public function __construct(string $apiKey, string $host, string $storeId, string $secret)
+  public function __construct(string $apiKey, string $host, string $storeId, string $currency = 'SATS')
   {
     $this->apiKey = $apiKey;
     $this->host = $host;
     $this->storeId = $storeId;
-    $this->secret = $secret;
+    $this->currency = $currency;
 
     // Create a new invoice client instance
     $this->invoice = new Invoice($this->host, $this->apiKey);
   }
+
   public function getInvoice($invoiceId): BTCPayServer\Result\Invoice
   {
     return $this->invoice->getInvoice($this->storeId, $invoiceId);
+  }
+
+  public function createInvoice(string $amount, string $redirectUrl = '', array $metadata = []): string
+  {
+    $invoiceAmount = PreciseNumber::parseString($amount);
+    $checkoutOptions = new InvoiceCheckoutOptions();
+    $checkoutOptions
+      ->setSpeedPolicy($checkoutOptions::SPEED_HIGH)
+      ->setExpirationMinutes(45)
+      ->setMonitoringMinutes(120)
+      ->setPaymentMethods([$this->currency, 'BTC'])
+      ->setPaymentTolerance(0)
+      ->setRedirectAutomatically(empty($redirectUrl) ? false : true)
+      ->setRedirectURL($redirectUrl);
+    try {
+      $invoice = $this->invoice->createInvoice(
+        $this->storeId,
+        $this->currency,
+        $invoiceAmount,
+        null,
+        null,
+        $metadata
+      );
+    } catch (Exception $e) {
+      error_log($e->getMessage());
+      return '';
+    }
+    return $invoice->getId();
   }
 }
