1bef12603a4b6112e2805c425d9918345ddb4b7a
Fishcake <me@fshck.com>
2023-07-31 19:09:39 +0900

    dirtyu fix of a type, need to make it much cleaner.

--- libs/NostrAuthMiddleware.class.php
+++ libs/NostrAuthMiddleware.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/NostrAuthHandler.class.php';
 require_once __DIR__ . '/Account.class.php';
 
