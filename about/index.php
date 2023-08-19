f46affd315dbc7f1bec8a8e92305a15e81c477e4
nostr.build <122138811+nostrbuild@users.noreply.github.com>
2023-08-19 16:58:57 -0400

    Fixed all links and minor updates

--- about/index.php
+++ about/index.php
@@ -31,7 +31,7 @@ $perm = new Permission();
     <link rel="stylesheet" href="/styles/index.css" />
     <link rel="stylesheet" href="/styles/header.css" />
     <link rel="stylesheet" href="/styles/tos.css" />
-    <link rel="icon" href="/assets/01.png" />
+    <link rel="icon" href="https://cdn.nostr.build/assets/01.png" />
 
     <title>nostr.build - about</title>
   </head>
@@ -277,43 +277,40 @@ $perm = new Permission();
       <h3>Short history</h3>
       <section class="paragraph">
         <p>
-          nostr.build started as a free, community focused, media (image, gif
+           nostr.build started as a free, community focused, media (image, gif
           and video) uploader for the nostr social media platform on December
-          24th, 2022. Check out our original website
-          <a class="ref_link" href="/v1">HERE</a> before
-          <a class="ref_link" href="/v1">@rob</a> provided the design updates!
+          24th, 2022. Check out our
+          <a class="ref_link" href="/v1">original website</a> before
+          <a class="ref_link" href="https://snort.social/p/npub1uapy44zhu5f0markfftt7m2z3gr2zwssq6h3lw8qlce0d5pjvhrs3q9pmv">@rob</a> and <a class="ref_link" href="https://snort.social/p/npub15s8w3yrmswtta4n7rkac47jetntyx6ar6lq4h73fsskqacwvsfdsvaallg">@nahuelg5</a> completely redesigned it!
           nostr.build’s main purpose is to help grow the nostr ecosystem by
-          providing a free web based, API or application integrated way to
+          providing a free, web based, API or application integrated way to
           upload user’s favorite media. We believe in a transparent, community
-          approach to media moderation and open source code
-          <a class="ref_link" href="https://github.com/nostrbuild/nostr.build"
-            >HERE</a
-          >.
+          approach to media moderation and <a class="ref_link" href="https://github.com/nostrbuild/nostr.build" target="_blank">open source code.</a>
         </p>
 
-        <img class="about_img" src="/assets/about/nostrbuild_v1.png" alt="nostr.build v1" height="300"/>
+        <a href="/v1" target="_blank"><img class="about_img" src="https://cdn.nostr.build/assets/about/nostrbuild_v1.png" alt="nostr.build v1" height="300"/></a>
       </section>
 
       <p>Some of nostr.build’s key principles include:</p>
       <p>
         - Always provide a free service that will keep uploaded media as
-        accurate and as long as possible.’Archived!’<br />
+        accurate and as long as possible, ’Archived!’<br />
         - No ads! No commercial advertisements!<br />
-        - Only community projects, nostr builders, creators, devs, artists and
+        - Only community projects, nostr builders, educators, creators, developers, artists and
         memes. Just ask us.<br />
         - Provide value for value to users including new features, integrations,
         support, and responsive communication.<br />
       </p>
       <h3>Free services</h3>
       <p>
-        Nostr.build’s key objective is to offer a free, no ads, reliable, long
+        nostr.build’s key objective is to offer a free, no ads, reliable, long
         time storage of media for the nostr community.<br />
         We will also continue to add additional features to our free service,
         including:<br />
 
-        - Image, gif, audio and video uploads up to 25MB hosted on the AWS S3<br />
+        - Image, gif, audio and video uploads up to 25MB+ hosted on the AWS S3<br />
         - Global, lightning fast CDN network for all images and GIFs using
-        Bunny.net<br />
+        <a class="ref_link" href="https://bunny.net" target="_blank">Bunny.net</a><br />
         - No ads! Only community projects, nostr builders, creators, devs,
         artists and memes<br />
         - Profile picture uploader that properly shrinks, crops and puts your PP
@@ -321,89 +318,89 @@ $perm = new Permission();
         - API for all nostr applications, projects and developers<br /><br />
 
         nostr.build was recently honored to be part of the first wave of nostr
-        devs to receive a grant from the OpenSats public charity focused on
-        Bitcoin and Nostr projects, HERE. Although this will help greatly with
+        devs to receive a grant from the <a class="ref_link" href="https://opensats.org/blog/nostr-grants-july-2023">OpenSats public charity</a> focused on
+        Bitcoin and Nostr projects. Although this will help greatly with
         R&D, new projects, growth and overages, the goal is to keep nostr.build
         ‘self-sustainable’ through account earnings.<br /><br />
         ** People often ask, “how do you cover the charges for your free
-        services? Paid Accounts.<br /><br />
+        services?" Paid Accounts.<br /><br />
       </p>
       <figure class="img_container double_img">
-        <img class="about_img img_horizontal" src="/assets/about/opensats.png" alt="opensats grant" height="80"/>
-        <img class="about_img img_horizontal" src="/assets/about/githublogo.png" alt="opensats grant" height="80"/>
+      <a href="https://opensats.org/blog/nostr-grants-july-2023" target="_blank"><img class="about_img img_horizontal" src="https://cdn.nostr.build/assets/about/opensats.png" alt="opensats grant" height="80"/></a>
+      <a href="https://github.com/nostrbuild/nostr.build" target="_blank"><img class="about_img img_horizontal" src="https://cdn.nostr.build/assets/about/githublogo.png" alt="GitHub" height="80"/></a>
       </figure>
       <h3>Paid accounts</h3>
       <section class="paragraph para_square">
-        <img class="about_img img_square" src="/assets/primo_nostr.png" alt="nostr.build account logo" width="80"/>
+      <a href="https://nostr.build/signup"><img class="about_img img_square" src="https://cdn.nostr.build/assets/primo_nostr.png" alt="nostr.build account logo" width="80"/></a>
         <p>
           nostr.build offers accounts with premium features, charged annually.
           Accounts can be purchased with a lighting wallet or Bitcoin. Proceeds
-          are kept on the pay.nostr.build lighting node HERE, and used to pay
-          for all nostr.build expenses: AWS, CDN, storage, domains, and other
-          related expenses.
+          are kept on the <a class="ref_link" href="https://amboss.space/node/02e869c409bd62ca84e9306ad96d9daef3b2b31a1c777b501fc55f2c09969ce1a3">pay.nostr.build lighting node</a> and used to pay
+          for all nostr.build expenses: AWS, CDN, storage, development, domains, and other related expenses.
         </p>
       </section>
 
       <p>
         ** Purchasing an account not only gives you premium features, you are
-        also supporting a free, ads-free, open source, service for all of nostr
+        also supporting a free, no ads, open source, service for all of nostr
         and decentralized social media!<br />
 
         Account features include:<br />
-        - Blazing fast global CDN (Bunny.net) on the AWS S3 for all images and
-        videos<br />
-        - Unlimited file-size for media uploads (up to your account size
-        5/10/20GB)<br />
+        - Blazing fast global CDN (Bunny.net) on the AWS S3 for all images and videos<br />
+        - Unlimited file-size for media uploads (up to your account size 5/10/20GB)<br />
         - Post you media to the Creators page on nostr.build<br />
         - Ability to easily ‘Delete’ media after posted<br />
         - Private folders not seen in the View All gallery<br />
-        - Access to View All free media ever uploaded to nostr.build, over 450k
-        images, gifs and videos!<br />
-        ** See all nostr.build Accounts and features, HERE!
+        - Access to View All free media ever uploaded to nostr.build, over 500k images, gifs and videos!<br />
+        - BTCPay Server account<br />
+        ** See all <a class="ref_link" href="https://nostr.build/signup">nostr.build Accounts</a> and features<br />
       </p>
       <figure class="img_container double_img">
-        <img class="about_img img_horizontal" src="/assets/about/awss3.png" alt="nostr.build account logo" width="180"/>
-        <img class="about_img img_horizontal" src="/assets/about/bunnynet.png" alt="nostr.build account logo" width="180"/>
+        <img class="about_img img_horizontal" src="https://cdn.nostr.build/assets/about/awss3.png" alt="nostr.build account logo" width="180"/>
+        <img class="about_img img_horizontal" src="https://cdn.nostr.build/assets/about/bunnynet.png" alt="nostr.build account logo" width="180"/>
       </figure>
 
       <h4>How to use Account related features:</h4>
       <section class="paragraph">
         <p>
-          - First, purchase the account with the features you would like,
-          HERE!<br />
-          - Folders: Click on ‘New Folder’ in the left menu bar, name your
-          folder and click Create.<br />
+          - First, purchase an account <a class="ref_link" href="https://nostr.build/signup">HERE!</a><br />
+          - Folders: Click on ‘New Folder’ in the left menu bar, name your folder and click Create.<br />
           - Move a file to a folder: ‘checkbox’ the media you want to move, and
           ‘Double Click’ on the folder you want to move it to.<br />
-          - Delete a file or folder: ‘checkbox’ the file or folder you want to
-          delete, and click on the red Delete button on the bottom left menu
-          bar.<br />
+          - Delete a file or folder: ‘checkbox’ the what you want to
+          delete, click on the red Delete button on the bottom left menu bar.<br />
           - ** If you delete a folder with files in it, those files just move to
           the ‘no folder’ section, they are NOT deleted.<br />
           - ‘View All’ allows you to see all free uploads ever made to
           nostr.build, excluding paid accounts and profile pictures. All images
-          must be legal and align to nostr.build Terms of Service.<br />
+          must be legal and align to nostr.build <a class="ref_link" href="https://nostr.build/tos/" target="_blank">Terms of Service.</a> <br />
           - If you purchased a Creator account, you can toggle your media to be
-          publically available on the nostr.build Creators page, HERE.<br />
-          - BTCPay Server Account:<br />
+          publically available on the <a class="ref_link" href="https://nostr.build/creators/" target="_blank">nostr.build Creators page.</a><br />
         </p>
 
         <figure class="img_container double_img">
-          <img src="/assets/about/createfolder.png" alt="create a folder" class="about_img" />
-          <img src="/assets/about/movedelete.png" alt="move and delete" class="about_img" />
+          <img src="https://cdn.nostr.build/assets/about/createfolder.png" alt="create a folder" class="about_img" />
+          <img src="https://cdn.nostr.build/assets/about/movedelete.png" alt="move and delete" class="about_img" />
         </figure>
       </section>
 
       <h3>Contacts</h3>
       <figure class="img_container double_img">
-        <a href="https://snort.social/p/npub1nxy4qpqnld6kmpphjykvx2lqwvxmuxluddwjamm4nc29ds3elyzsm5avr7"><img src="/assets/about/nostrbuild.png" alt="nostr.build" class="about_img img_square"/></a>
+        <a href="https://snort.social/p/npub1nxy4qpqnld6kmpphjykvx2lqwvxmuxluddwjamm4nc29ds3elyzsm5avr7"><img src="https://cdn.nostr.build/assets/about/nostrbuild.png" alt="nostr.build" class="about_img img_square"/></a>
         <img src="/assets/about/community.png" alt="community" class="about_img img_square" />
-        <a href="https://snort.social/p/npub137c5pd8gmhhe0njtsgwjgunc5xjr2vmzvglkgqs5sjeh972gqqxqjak37w"><img src="/assets/about/fishcake.png" alt="fishcake" class="about_img img_square" /></a>
+        <a href="https://snort.social/p/npub137c5pd8gmhhe0njtsgwjgunc5xjr2vmzvglkgqs5sjeh972gqqxqjak37w"><img src="https://cdn.nostr.build/assets/about/fishcake.png" alt="fishcake" class="about_img img_square" /></a>
       </figure>
     </main>
 
     <?= include $_SERVER['DOCUMENT_ROOT']?>
     <script src="/scripts/index.js"></script>
-    <br>
+
+    <footer>
+			<nav>
+				<div class="footer_made_by">
+					nostr.build 2023
+				</div>
+			</nav>
+		</footer>
   </body>
 </html>
