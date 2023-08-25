c59cd84b1eaa15b8e81780989ed2f9cb254d4d15
nostr.build <122138811+nostrbuild@users.noreply.github.com>
2023-08-25 08:44:37 -0700

    updated footer

--- components/footer.php
+++ components/footer.php
@@ -1,42 +1,31 @@
 <footer>
 			<nav>
-				<div class="footer_made_by">
-					nostr.build 2023
-					<a href="https://iris.to/nostr.build" class="contact" target="_blank">
-						<img src="/assets/footer_made.png" alt="nostr.build image" />
-						Contact us
-					</a>
-				</div>
-
 				<div class="footer_contributors">
-					Key contributors
+					Contact us
 					<ul class="contributors_list">
-						<li class="contributor">
-							<a href="https://iris.to/fishcake@nostrpix.com" target="_blank">
-								<img src="/assets/contributors/6.png" alt="" />
+					<li class="contributor">
+							<a href="https://nostr.com/npub1nxy4qpqnld6kmpphjykvx2lqwvxmuxluddwjamm4nc29ds3elyzsm5avr7" target="_blank">
+								<img src="https://cdn.nostr.build/assets/footer_made.png" alt="" />
 							</a>
 						</li>
 						<li class="contributor">
-							<a href="https://iris.to/walker@primal.net" target="_blank">
-								<img src="/assets/contributors/1.png" alt="" />
+							<a href="https://nostr.com/npub137c5pd8gmhhe0njtsgwjgunc5xjr2vmzvglkgqs5sjeh972gqqxqjak37w" target="_blank">
+								<img src="https://cdn.nostr.build/assets/contributors/6.png" alt="" />
 							</a>
 						</li>
-						<li class="contributor">
-							<a href="https://iris.to/nostrstuff.com" target="_blank">
-								<img src="/assets/contributors/4.png" alt="" />
-							</a>
 						</li>
 						<li class="contributor">
-							<a href="https://iris.to/rob@nostrplebs.com" target="_blank">
-								<img src="/assets/contributors/5.png" alt="" />
-							</a>
-						</li>
-						<li class="contributor">
-							<a href="https://github.com/ng5jr" target="_blank">
-								<img src="/assets/contributors/github.png" alt="" />
+							<a href="https://github.com/nostrbuild/nostr.build" target="_blank">
+								<img src="https://cdn.nostr.build/assets/contributors/github.png" alt="" />
 							</a>
 						</li>
 					</ul>
 				</div>
+
+				<div class="footer_made_by">
+					nostr.build 2023
+				</div>
+
+
 			</nav>
 		</footer>
