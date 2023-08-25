8081c69ea0322a6ba05b265c0da2262a0e47a2c6
nostr.build <122138811+nostrbuild@users.noreply.github.com>
2023-08-25 11:02:23 -0700

    cleaned up footer

--- components/footer.php
+++ components/footer.php
@@ -1,31 +1,27 @@
 <footer>
-			<nav>
-				<div class="footer_contributors">
-					Contact us
-					<ul class="contributors_list">
+		<nav>
+			<div class="footer_contributors">
+				Contact us
+				<ul class="contributors_list">
 					<li class="contributor">
-							<a href="https://nostr.com/npub1nxy4qpqnld6kmpphjykvx2lqwvxmuxluddwjamm4nc29ds3elyzsm5avr7" target="_blank">
-								<img src="https://cdn.nostr.build/assets/footer_made.png" alt="" />
-							</a>
-						</li>
-						<li class="contributor">
-							<a href="https://nostr.com/npub137c5pd8gmhhe0njtsgwjgunc5xjr2vmzvglkgqs5sjeh972gqqxqjak37w" target="_blank">
-								<img src="https://cdn.nostr.build/assets/contributors/6.png" alt="" />
-							</a>
-						</li>
-						</li>
-						<li class="contributor">
-							<a href="https://github.com/nostrbuild/nostr.build" target="_blank">
-								<img src="https://cdn.nostr.build/assets/contributors/github.png" alt="" />
-							</a>
-						</li>
-					</ul>
-				</div>
-
-				<div class="footer_made_by">
+						<a href="https://nostr.com/npub1nxy4qpqnld6kmpphjykvx2lqwvxmuxluddwjamm4nc29ds3elyzsm5avr7" target="_blank">
+							<img src="https://cdn.nostr.build/assets/footer_made.png" alt="" />
+						</a>
+					</li>
+					<li class="contributor">
+						<a href="https://nostr.com/npub137c5pd8gmhhe0njtsgwjgunc5xjr2vmzvglkgqs5sjeh972gqqxqjak37w" target="_blank">
+							<img src="https://cdn.nostr.build/assets/contributors/6.png" alt="" />
+						</a>
+					</li>
+					<li class="contributor">
+						<a href="https://github.com/nostrbuild/nostr.build" target="_blank">
+							<img src="https://cdn.nostr.build/assets/contributors/github.png" alt="" />
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="footer_made_by">
 					nostr.build 2023
-				</div>
-
-
-			</nav>
-		</footer>
+			</div>
+		</nav>
+</footer>
