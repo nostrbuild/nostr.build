ac9a6cc03bc813519cf46502683e068b53857023
nostr.build <122138811+nostrbuild@users.noreply.github.com>
2023-09-25 12:09:12 -0700

    Fixed issue with tshirt ad too large on some pages
    Moved ad into <nav> and changed to style="width:180px;"

--- components/mainnav.php
+++ components/mainnav.php
@@ -145,5 +145,5 @@ SVG;
       </a>
     <?php endif; ?>
   </div>
+  <a class="ref_link" style="font-size: large" href="https://btcpay.nostr.build/apps/5HcE4o7M56JrgrzpHSeSzNrK7Ss/pos"> Bitpopart <img src="https://cdn.nostr.build/assets/images/nostrasia01.png" style="width:180px;" alt="nostrasia"> T-Shirts HERE!</a>
 </nav>
-<a class="ref_link" style="font-size: large" href="https://btcpay.nostr.build/apps/5HcE4o7M56JrgrzpHSeSzNrK7Ss/pos"> Bitpopart <img src="https://cdn.nostr.build/assets/images/nostrasia01.png" width="180" alt="nostrasia"> T-Shirts HERE!</a>
