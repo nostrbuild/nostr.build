cdab5a950d7357b0f183748b532486ddb2dc7791
Fishcake <me@fshck.com>
2023-11-05 14:27:23 +0900

    make header.js safer and not error when html is not present

--- scripts/header.js
+++ scripts/header.js
@@ -1,10 +1,17 @@
-document.querySelector(".menu_button").addEventListener("click", () => {
-  if (document.querySelector(".menu").style.display === "flex") {
+const menu_button = document.querySelector(".menu_button");
+if (menu_button) {
+  menu_button.addEventListener("click", () => {
+    if (document.querySelector(".menu").style.display === "flex") {
+      document.querySelector(".menu").style.display = "none";
+    } else {
+      document.querySelector(".menu").style.display = "flex";
+    }
+  });
+}
+
+const main_menu = document.querySelector(".main");
+if (main_menu) {
+  main_menu.addEventListener("click", () => {
     document.querySelector(".menu").style.display = "none";
-  } else {
-    document.querySelector(".menu").style.display = "flex";
-  }
-});
-document.querySelector("main").addEventListener("click", () => {
-  document.querySelector(".menu").style.display = "none";
-});
+  });
+}
