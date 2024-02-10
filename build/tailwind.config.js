f9c2afc6634f5381abe446e1b828c05ea9351085
Fishcake <me@fshck.com>
2024-02-10 16:17:51 +0900

    add custom purple to tw config

--- build/tailwind.config.js
+++ build/tailwind.config.js
@@ -9,6 +9,21 @@ module.exports = {
     "!./vendor/",
   ],
   theme: {},
+  "colors": {
+    "purple": {
+      50: "#F9EEFB",
+      100: "#F2DDF8",
+      200: "#E7C0F2",
+      300: "#DA9EEB",
+      400: "#CD7CE3",
+      500: "#C25EDD",
+      600: "#AB2CCE",
+      700: "#81219C",
+      800: "#571669",
+      900: "#2A0B33",
+      950: "#150519"
+    }
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
