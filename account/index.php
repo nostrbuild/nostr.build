2b82aa570d283cb1b8158935c05962a60df7a4f0
Fishcake <me@fshck.com>
2024-02-25 23:31:57 +0900

    add renew and upgrade button to account page

--- account/index.php
+++ account/index.php
@@ -51,6 +51,9 @@ if ($daysRemaining <= 0) {
 	exit;
 }
 
+$showRenewalButton = $daysRemaining <= 30;
+$showUpgradeButton = $acctlevel < 10 && !$showRenewalButton;
+
 // Fetch user's folder statistics and storage statistics
 $usersFoldersTable = new UsersImagesFolders($link);
 $usersFoldersStats = $usersFoldersTable->getFoldersStats($user);
@@ -78,7 +81,7 @@ $userStorageRemaining = $userOverLimit ? 0 : $userStorageLimit - $storageUsed;
 
 	<link rel="stylesheet" href="/styles/account.css?v=5" />
 	<link href="/scripts/dist/index.css?v=19" rel="stylesheet">
-	<link href="/styles/twbuild.css?v=46" rel="stylesheet">
+	<link href="/styles/twbuild.css?v=47" rel="stylesheet">
 	<link rel="icon" href="/assets/primo_nostr.png" />
 
 	<script defer src="/scripts/dist/index.js?v=35"></script>
@@ -427,13 +430,28 @@ $userStorageRemaining = $userOverLimit ? 0 : $userStorageLimit - $storageUsed;
 
 
 		<section class="dashboard_section">
-			<h3 class="dashboard_title">Welcome to your
-
+			<h3 class="dashboard_title">Welcome <b><?= $nym ?? 'anon' ?></b>!
 				<?php
+				/*
 				echo SiteConfig::getAccountType($acctlevel) . ', ';
 				echo '<b>' . ($nym ?? 'anon') . '</b>';
+				*/
 				?>
-
+				<?php if ($showRenewalButton) : ?>
+				<button @click="window.location.href='/plans/'" type="button" class="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+					<svg class="-ml-0.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+						<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+					</svg>
+					Renew
+				</button>
+				<?php elseif ($showUpgradeButton) : ?>
+				<button @click="window.location.href='/plans/'" type="button" class="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+					<svg class="-ml-0.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+						<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+					</svg>
+					Upgrade
+				</button>
+				<?php endif; ?>
 			</h3>
 			<div class="dashboard_info">
 				<div class="plan_data">
@@ -442,7 +460,8 @@ $userStorageRemaining = $userOverLimit ? 0 : $userStorageLimit - $storageUsed;
 						<p style="text-align: left;">
 							<b>Remaining Days:</b> <?= $daysRemaining ?>
 						</p>
-						<p><b><?= formatSizeUnits($usersFoldersStats['TOTAL']['totalSize']) ?></b> /
+						<p>
+							<b><?= formatSizeUnits($usersFoldersStats['TOTAL']['totalSize']) ?></b> /
 							<?= htmlentities(SiteConfig::getStorageLimitMessage($acctlevel)) ?>
 							<?= $userOverLimit ? '<br><span style="color: red;">Max storage reached! Delete images or upgrade plan for more space.</span><br>' : '' ?>
 						</p>
@@ -542,6 +561,7 @@ $userStorageRemaining = $userOverLimit ? 0 : $userStorageLimit - $storageUsed;
 					$images_result = $images_stmt->get_result();
 
 					// TODO: This is definetly going away, just a temp blaspheme until rewrite
+					// There is nothing more permanent tham a temporary solution ;)
 					$i = 0;
 					$divId = 0;
 
