88b8a3d949b775a298ef457f872cfbf2bc59ecdb
Fishcake <me@fshck.com>
2024-05-06 18:11:30 +0900

    adjust profile edit and logout icons

--- account/index.php
+++ account/index.php
@@ -77,15 +77,30 @@ $pageMenuContent = <<<HTML
 					</div>
 				</div>
 				<div class="flex items-center">
-					<button @click.prevent="profileStore.openDialog(); menuStore.mobileMenuOpen = false; menuStore.disableDeleteFolderButtons(); " class="text-sm font-semibold leading-6 text-nbpurple-50 hover:bg-nbpurple-800 p-2 rounded-md">
+					<button @click.prevent="profileStore.openDialog(); menuStore.mobileMenuOpen = false; menuStore.disableDeleteFolderButtons(); " class="text-sm font-semibold leading-6 text-nbpurple-50 hover:bg-nbpurple-800 p-1.5 rounded-md">
+						<!--
 						<svg aria-hidden="true" class="size-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 							<path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
 							<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
 						</svg>
+-->
+						<svg aria-hidden="true" class="size-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+							<path d="M2 21a8 8 0 0 1 10.434-7.62" />
+							<circle cx="10" cy="8" r="5" />
+							<circle cx="18" cy="18" r="3" />
+							<path d="m19.5 14.3-.4.9" />
+							<path d="m16.9 20.8-.4.9" />
+							<path d="m21.7 19.5-.9-.4" />
+							<path d="m15.2 16.9-.9-.4" />
+							<path d="m21.7 16.5-.9.4" />
+							<path d="m15.2 19.1-.9.4" />
+							<path d="m19.5 21.7-.4-.9" />
+							<path d="m16.9 15.2-.4-.9" />
+						</svg>
 						<span class="sr-only">Profile Settings</span>
 					</button>
-					<button @click.prevent="logoutScreen = true" class="text-sm font-semibold leading-6 text-nbpurple-50 hover:bg-nbpurple-800 p-2 rounded-md">
-						<svg aria-hidden="true" class="size-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<button @click.prevent="logoutScreen = true" class="text-sm font-semibold leading-6 text-nbpurple-50 hover:bg-nbpurple-800 p-1.5 rounded-md">
+						<svg aria-hidden="true" class="size-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
 							<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
 						</svg>
 						<span class="sr-only">Logout</span>
