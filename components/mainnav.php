a31b28811c2a8921936851049e611ce4d246870b
nostr.build <122138811+nostrbuild@users.noreply.github.com>
2024-11-29 09:39:06 -0500

    Added Black Friday banner on home page

--- components/mainnav.php
+++ components/mainnav.php
@@ -163,4 +163,5 @@ SVG;
       </a>
     <?php endif; ?>
   </div>
+      <a class="ref_link" style="font-size: large" href="https://nostr.build/plans/"><img src="https://i.nostr.build/7VBe.png" style="width:220px;" alt="Premium Accounts"></a>
 </nav>
